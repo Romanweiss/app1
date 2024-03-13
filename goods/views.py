7ff8ef52babd28f7f278914035627994e4e1b933
romanweiss <romanweib@gmail.com>
2024-03-13 15:59:10 +0300

    product filter, urlencode

--- goods/views.py
+++ goods/views.py
@@ -7,13 +7,21 @@ from goods.models import Products
 
 def catalog(request, category_slug):
 
-    page = request.GET.get('page', 1)
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
-    if category_slug == 'all':
+    if category_slug == "all":
         # получаем информацию из бд в отдельный файл
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     # создаём пагинацию
     paginator = Paginator(goods, 3)
@@ -22,7 +30,7 @@ def catalog(request, category_slug):
     context = {
         "title": "Home - каталог",
         "goods": current_page,
-        'slug_url': category_slug
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
@@ -31,8 +39,6 @@ def product(request, product_slug):
 
     product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
     return render(request, "goods/product.html", context=context)
