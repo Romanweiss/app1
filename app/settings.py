d54a215329c5a1250ace84b9c3ceda3666706d01
romanweiss <romanweib@gmail.com>
2024-03-15 00:13:12 +0300

    Postgres SQL db is connected, full django text search is added

--- app/settings.py
+++ app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',  # приложение для поиска и обслуживания статических файлов
+    'django.contrib.postgres',  # подключаем для поиска
 
     'debug_toolbar', # приложение - тулбар для отслеживания sql запросов прямо на сайте
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -8,12 +9,18 @@ def q_search(query):
     if query.isdigit() and len(query) <= 7:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    # поиск из коробки django - намного лучше вариант, чем самопальный, тк учитываются падежи и прочие искажения
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
 
-    q_objects = Q()
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # q_objects = Q()
 
-    return Products.objects.filter(q_objects)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
