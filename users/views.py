b8c8840ced98bd4e085262f609382b517f259045
romanweiss <romanweib@gmail.com>
2024-03-15 16:23:05 +0300

    Reg form & view func are ready

--- users/views.py
+++ users/views.py
@@ -1,46 +1,54 @@
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
-            
+                return HttpResponseRedirect(reverse("main:index"))
+
     else:
         form = UserLoginForm()
-    
 
-    context = {
-        'title': 'Home - Авторизация',
-        'form': form
-    }
-    return render(request, 'users/login.html', context)
+    context = {"title": "Home - Авторизация", "form": form}
+    return render(request, "users/login.html", context)
 
 
 def registration(request):
-    context = {
-        'title': 'Home - Регистрация'
-    }
-    return render(request, 'users/registration.html', context)
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            # Ниже код для того чтобы сразу после регистрации пользователь залогинился
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+
+    else:
+        form = UserRegistrationForm()
+
+    context = {"title": "Home - Регистрация", "form": form}
+    return render(request, "users/registration.html", context)
 
 
 def profile(request):
-    context = {
-        'title': 'Home - Кабинет'
-    }
-    return render(request, 'users/profile.html', context)
+    context = {"title": "Home - Кабинет"}
+    return render(request, "users/profile.html", context)
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
+
+
+
