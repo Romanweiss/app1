6abc61adefe66a3b83ad8199ee4b232c3bd3b165
romanweiss <romanweib@gmail.com>
2024-03-17 16:37:57 +0300

    add to cart request & json response

--- goods/models.py
+++ goods/models.py
@@ -1,57 +1,53 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Categories(models.Model):
-    name = models.CharField("Название категории", max_length=150, unique=True)
-    slug = models.SlugField("URL", max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
-        db_table = (
-            "category"  # название таблицы, которое будет отображаться в СУБД (ед.число)
-        )
-        verbose_name = "категорию"  # отвечает на вопрос - кого? чего? в ед. числе
-        verbose_name_plural = "Категории"  # множественное число
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
 
-    # переопределяем метод, для адекватного отображения объектов в админке
     def __str__(self):
         return self.name
 
 
 class Products(models.Model):
-    name = models.CharField("Название", max_length=150, unique=True)
-    slug = models.SlugField("URL", max_length=200, unique=True, blank=True, null=True)
-    description = models.TextField("Описание", blank=True, null=True)
-    image = models.ImageField(
-        "Изображение", upload_to="goods_images", blank=True, null=True
-    )
-    price = models.DecimalField("Цена", default=0.00, max_digits=7, decimal_places=2)
-    discount = models.DecimalField(
-        "Скидка в процентах", default=0.00, max_digits=4, decimal_places=2
-    )
-    quantity = models.PositiveIntegerField("Количество", default=0)
-
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     # Ниже связываем таблицы Categories & Products
-    category = models.ForeignKey(
-        to=Categories, on_delete=models.CASCADE, verbose_name="Категория"
-    )
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
 
     class Meta:
-        db_table = "product"
-        verbose_name = "продукт"  # отвечает на вопрос - кого? чего? в ед. числе
-        verbose_name_plural = "Продукты"  # множественное число
-        ordering = ('id',) # сортировка
+        db_table = 'product'
+        verbose_name = 'Продукт' # отвечает на вопрос - кого? чего? в ед. числе
+        verbose_name_plural = 'Продукты' # множественное число
+        ordering = ("id",) # сортировка
 
-    # переопределяем метод, для адекватного отображения объектов в админке
+# переопределяем метод, для адекватного отображения объектов в админке
     def __str__(self):
-        return f"{self.name} Количество - {self.quantity}"
+        return f'{self.name} Количество - {self.quantity}'
 
-    # метод для преобразования id
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
+# метод для преобразования id
     def display_id(self):
         return f"{self.id:05}"
 
-    # метод для расчёта цены на товар
+
+ # метод для расчёта цены на товар
     def sell_price(self):
         if self.discount:
-            return round(self.price - self.price * self.discount / 100, 2)
+            return round(self.price - self.price*self.discount/100, 2)
         
         return self.price
