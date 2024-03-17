941905fba9251bc33a3a31703907e940252ccc7e
romanweiss <romanweib@gmail.com>
2024-03-18 02:55:21 +0300

    Create order

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from carts.admin import CartTabAdmin
 from goods.models import Categories
+from orders.admin import OrderTabulareAdmin
 
 from users.models import User
 
@@ -11,4 +12,4 @@ class Usermin(admin.ModelAdmin):
     list_display = ['username','first_name', 'last_name', 'email']
     search_fields = ['username','first_name', 'last_name', 'email']
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
